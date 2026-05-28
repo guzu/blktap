dbe4b836ce716ea3c4f4fc9fe33e576ea78c71ac
Emmanuel Varagnat <emmanuel.varagnat@vates.tech>
2026-05-28 17:52:26 +0200

    DBG: more counters: thread mutex, aio_poll, lockcnt

--- qcow2/lib/qcow2-counters.c
+++ qcow2/lib/qcow2-counters.c
@@ -13,13 +13,26 @@ uint64_t qcow2_counter_cache_get_read;
 uint64_t qcow2_counter_cache_get_replace_entry;
 uint64_t qcow2_counter_coroutine_yield;
 uint64_t qcow2_counter_coroutine_enter;
+uint64_t qcow2_counter_coroutine_terminate;
 uint64_t qcow2_counter_co_mutex_lock_entry;
 uint64_t qcow2_counter_co_mutex_lock_return;
 uint64_t qcow2_counter_co_mutex_lock_uncontended;
+uint64_t qcow2_counter_co_mutex_unlock_entry;
+uint64_t qcow2_counter_co_mutex_unlock_return;
 uint64_t qcow2_counter_aio_co_schedule;
 uint64_t qcow2_counter_aio_co_schedule_bh_cb;
 uint64_t qcow2_counter_bdrv_co_preadv;
 uint64_t qcow2_counter_bdrv_co_pwritev;
+uint64_t qcow2_counter_mutex_lock;
+uint64_t qcow2_counter_mutex_locked;
+uint64_t qcow2_counter_mutex_unlock;
+uint64_t qcow2_counter_run_poll_handlers_begin;
+uint64_t qcow2_counter_run_poll_handlers_end;
+uint64_t qcow2_counter_poll_grow;
+uint64_t qcow2_counter_poll_shrink;
+uint64_t qcow2_counter_lockcnt_fast_path_attempt;
+uint64_t qcow2_counter_lockcnt_fast_path_success;
+uint64_t qcow2_counter_lockcnt_futex_wait;
 
 void qcow2_counters_dump(FILE *out)
 {
@@ -31,9 +44,12 @@ void qcow2_counters_dump(FILE *out)
     DUMP(bdrv_co_pwritev);
     DUMP(coroutine_enter);
     DUMP(coroutine_yield);
+    DUMP(coroutine_terminate);
     DUMP(co_mutex_lock_entry);
     DUMP(co_mutex_lock_uncontended);
     DUMP(co_mutex_lock_return);
+    DUMP(co_mutex_unlock_entry);
+    DUMP(co_mutex_unlock_return);
     DUMP(aio_co_schedule);
     DUMP(aio_co_schedule_bh_cb);
     DUMP(cache_get);
@@ -41,6 +57,16 @@ void qcow2_counters_dump(FILE *out)
     DUMP(cache_get_replace_entry);
     DUMP(thread_pool_submit);
     DUMP(thread_pool_complete);
+    DUMP(mutex_lock);
+    DUMP(mutex_locked);
+    DUMP(mutex_unlock);
+    DUMP(run_poll_handlers_begin);
+    DUMP(run_poll_handlers_end);
+    DUMP(poll_grow);
+    DUMP(poll_shrink);
+    DUMP(lockcnt_fast_path_attempt);
+    DUMP(lockcnt_fast_path_success);
+    DUMP(lockcnt_futex_wait);
 #undef DUMP
 
     /* Derived stats */
@@ -60,6 +86,26 @@ void qcow2_counters_dump(FILE *out)
         fprintf(out, "  %-40s %20.2f %%\n",
                 "cache miss rate", 100.0 * (double)cr / cg);
     }
+
+    uint64_t ml = __atomic_load_n(&qcow2_counter_mutex_lock, __ATOMIC_RELAXED);
+    uint64_t mk = __atomic_load_n(&qcow2_counter_mutex_locked, __ATOMIC_RELAXED);
+    if (ml > 0) {
+        /* mutex_lock counts attempts (uncontended path increments only here);
+         * mutex_locked counts the slow path that had to wait. */
+        fprintf(out, "  %-40s %20.2f %%\n",
+                "thread mutex slow-path rate",
+                100.0 * (double)mk / ml);
+    }
+
+    uint64_t la = __atomic_load_n(&qcow2_counter_lockcnt_fast_path_attempt,
+                                  __ATOMIC_RELAXED);
+    uint64_t ls = __atomic_load_n(&qcow2_counter_lockcnt_fast_path_success,
+                                  __ATOMIC_RELAXED);
+    if (la > 0) {
+        fprintf(out, "  %-40s %20.2f %%\n",
+                "lockcnt fast-path success rate",
+                100.0 * (double)ls / la);
+    }
 }
 
 void qcow2_counters_reset(void)
@@ -69,9 +115,12 @@ void qcow2_counters_reset(void)
     ZERO(bdrv_co_pwritev);
     ZERO(coroutine_enter);
     ZERO(coroutine_yield);
+    ZERO(coroutine_terminate);
     ZERO(co_mutex_lock_entry);
     ZERO(co_mutex_lock_uncontended);
     ZERO(co_mutex_lock_return);
+    ZERO(co_mutex_unlock_entry);
+    ZERO(co_mutex_unlock_return);
     ZERO(aio_co_schedule);
     ZERO(aio_co_schedule_bh_cb);
     ZERO(cache_get);
@@ -79,5 +128,15 @@ void qcow2_counters_reset(void)
     ZERO(cache_get_replace_entry);
     ZERO(thread_pool_submit);
     ZERO(thread_pool_complete);
+    ZERO(mutex_lock);
+    ZERO(mutex_locked);
+    ZERO(mutex_unlock);
+    ZERO(run_poll_handlers_begin);
+    ZERO(run_poll_handlers_end);
+    ZERO(poll_grow);
+    ZERO(poll_shrink);
+    ZERO(lockcnt_fast_path_attempt);
+    ZERO(lockcnt_fast_path_success);
+    ZERO(lockcnt_futex_wait);
 #undef ZERO
 }
